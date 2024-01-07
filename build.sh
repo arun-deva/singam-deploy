aa58b331646f2e40fbd59d6eb1176eb17b5f101d
arun-deva <fake>
2024-01-06 21:14:44 -0500

    Added guess that song -to k8s manifest

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VERSION=1.0.2
-docker build -t singam-nginx:$VERSION .
+IMAGE=singam-nginx
+docker build -t $IMAGE:$VERSION .
 
 if [ $? -ne 0 ]; then
   echo "Exiting"
@@ -8,15 +9,15 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "tagging the image with the exact name as the remote image as docker push requires"
-docker tag singam-nginx:$VERSION arundeva/singam-nginx:$VERSION
+docker tag $IMAGE:$VERSION arundeva/$IMAGE:$VERSION
 
 if [ $? -ne 0 ]; then
   echo "Exiting"
   exit 1
 fi
 
-echo "pushing arundeva/singam-nginx:$VERSION"
-docker push arundeva/singam-nginx:$VERSION
+echo "pushing arundeva/$IMAGE:$VERSION"
+docker push arundeva/$IMAGE:$VERSION
 
 if [ $? -ne 0 ]; then
   echo "Did you login to remote repo using the 'docker login' command?"
